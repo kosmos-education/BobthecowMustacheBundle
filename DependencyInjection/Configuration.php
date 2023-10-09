c3c35f69eae0156c5d8c7437535a7c8aeae6bcf7
Olivier Levasseur <olivier.levasseur@kosmos.fr>
2023-10-09 11:38:25 +0200

    Passage à symfony 5

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -27,8 +27,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('mustache');
+        $treeBuilder = new TreeBuilder('mustache');
+        $rootNode = $treeBuilder->getRootNode();
 
         // $rootNode->setInfo('Mustache Configuration');
 
